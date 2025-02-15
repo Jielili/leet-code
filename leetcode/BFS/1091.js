5e682e19cb99f50e170874eb37825481cb702a48
jielili <jielilifz@163.com>
2025-02-15 21:44:12 +0800

    feat: bfs

--- leetcode/BFS/1091.js
+++ leetcode/BFS/1091.js
@@ -2,48 +2,27 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var shortestPathBinaryMatrix = function (grid) {
-    const best = bfs(grid, [], 0, 0)
-    return best?.length || -1
-};
-
-var bfs = function (grid, path, x, y) {
-    const n = grid.length
-    if (x < 0 || y < 0 || x >= n || y >= n || grid[x][y] === 1 || path.indexOf(''+x+y)>=0) {
-        return false
-    }
-
-    path.push(''+x+y)
-
-    // 如果已经到右下角，返回path
-    if (x === n - 1 && y === n - 1) {
-        return path
-    }
-
-    const dirs = []
-    dirs.push(bfs(grid, [...path], x, y + 1))
-    dirs.push(bfs(grid, [...path], x + 1, y + 1))
-    dirs.push(bfs(grid, [...path], x + 1, y))
-    dirs.push(bfs(grid, [...path], x + 1, y - 1))
-    dirs.push(bfs(grid, [...path], x, y - 1))
-    dirs.push(bfs(grid, [...path], x - 1, y - 1))
-    dirs.push(bfs(grid, [...path], x - 1, y))
-    dirs.push(bfs(grid, [...path], x - 1, y + 1))
-
-
-    let best_path = false, best_path_steps = Number.MAX_VALUE
-    for (let item of dirs) {
-        if (!item) continue
-        if (item.length < best_path_steps) {
-            best_path = item
-            best_path_steps = item.length
+var shortestPathBinaryMatrix = function(grid) {
+    const n = grid.length - 1
+    if(grid[0][0] || grid[n][n]) return -1
+    const queue = [[0, 0, 1]]
+    const directions = [[0,1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1,0], [-1, 1]]
+    while(queue.length) {
+        const [cur_x, cur_y, steps] = queue.shift()
+        if(cur_x === n && cur_y===n) return steps
+        for(let i = 0;i< 8;i++) {
+            const [dx, dy] = directions[i]
+            const x = cur_x + dx, y = cur_y + dy
+            if(x>=0 && y>=0 && x<= n && y<= n && grid[x][y] === 0) {
+                queue.push([x, y, steps + 1])
+                grid[x][y] = 1
+            }
         }
     }
-
-    return best_path
-}
-
+    return -1
+};
 
 console.log(shortestPathBinaryMatrix([[0,1],[1,0]]))
 console.log(shortestPathBinaryMatrix([[0,0,0],[1,1,0],[1,1,0]]))
 console.log(shortestPathBinaryMatrix([[1,0,0],[1,1,0],[1,1,0]]))
+console.log(shortestPathBinaryMatrix([[[0,0,0],[1,1,0],[1,1,1]]]))
