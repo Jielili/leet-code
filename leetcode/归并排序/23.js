a82062396e5848c48b2484b5ee9e4cda8d098f9f
Jlili <lili.jie@funplus.biz>
2025-02-17 19:15:33 +0800

    feat: 23

---
+++ leetcode/归并排序/23.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+}
+ */
+
+function ListNode(val, next) {
+	this.val = (val===undefined ? 0 : val)
+	this.next = (next===undefined ? null : next)
+}
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+	
+};
+
+var merge = function () {
+	
+}
+
+console.log(mergeKLists([{val: 2, next: null}, null, {val: -1, next: null}]))
