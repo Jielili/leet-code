87f525a58584e1f24cbe78a51b1b49d09f0befb5
Jlili <lili.jie@funplus.biz>
2025-02-18 19:09:40 +0800

    feat: 合并K个升序链表

--- leetcode/归并排序/23.js
+++ leetcode/归并排序/23.js
@@ -14,12 +14,38 @@ function ListNode(val, next) {
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
-	
+var mergeKLists = function (lists) {
+    const len = lists.length
+    if (len === 1) return lists[0]
+    if (len === 2) return merge(lists[0], lists[1])
+	return mergeKLists([mergeKLists(lists.slice(0, len>>1)), mergeKLists(lists.slice((len>>1), len))])
 };
 
-var merge = function () {
-	
+var merge = function (list1, list2) {
+    let p = list1, q = list2
+    if (!p && !q) return null
+    const res = new ListNode()
+    let cur = res
+    while (p || q) {
+        if (!p) {
+            cur.next = q
+            break
+        }
+        if (!q) {
+            cur.next = p
+            break
+        }
+        if (p.val < q.val) {
+            cur.next = p
+            cur = cur.next
+            p = p.next
+        } else {
+            cur.next = q
+            cur = cur.next
+            q = q.next
+        }
+    }
+    return res.next
 }
 
 console.log(mergeKLists([{val: 2, next: null}, null, {val: -1, next: null}]))
