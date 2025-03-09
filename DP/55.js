7921bfdcc9aceb6dc9142268a0862651ff9c3333
jielili <jielilifz@163.com>
2025-03-09 22:54:47 +0800

    feat: 55. 跳跃游戏

--- DP/55.js
+++ DP/55.js
@@ -3,15 +3,9 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    let left = 0, right = 0
-    const len = nums.length
-    while(right < len) {
-        let max_right = 0
-        for(let i = left; i <= right; i++) {
-            max_right = Math.max(max_right, right + nums[i])
-        }
-        left = right + 1
-        right = max_right
+    let max = 0;
+    for(let i = 0; i<max; i++) {
+        max = Math.max(max, i + nums[i])
     }
-    return max_right >= len - 1
+    return max >= nums.length - 1
 };
