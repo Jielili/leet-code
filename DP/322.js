75fe43236a586cedf1b73635a49146047b55c86f
Jlili <lili.jie@funplus.biz>
2025-03-14 17:09:32 +0800

    feat: 322. 零钱兑换

--- DP/322.js
+++ DP/322.js
@@ -3,7 +3,31 @@
  * @param {number} amount
  * @return {number}
  */
+
+
 var coinChange = function (coins, amount) {
+    
+};
+
+
+var DFS = function (coins, amount) {
+    if (amount < 0) {
+        return -1
+    }
+    if (amount === 0) {
+        return 0
+    }
+    for (let i = 0; i < coins; i++) {
+        const left = DFS(coins, amount - coins[i])
+        if (left !== -1) {
+            return left + 1
+        }
+    }
+    return -1
+}
+
+
+var coinChange1 = function (coins, amount) {
     const dp = new Array(amount + 1).fill(amount + 1)
     dp[0] = 0
     for (let i = 0; i <= amount; i++) {
