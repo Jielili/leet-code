9d81b2932342ed6e9b99dd19e8cee58ddfde4657
Jlili <lili.jie@funplus.biz>
2025-03-17 17:02:41 +0800

    feat: 152. 乘积最大子数组

--- DP/152.js
+++ DP/152.js
@@ -5,7 +5,33 @@
 var maxProduct = function (nums) {
     const len = nums.length
     let res = Number.NEGATIVE_INFINITY
-    for (let i = 0, max = 1, min = 1; i < len; i++) {
+    let p = 1, q = 1, found = false
+    for (let i = 0; i < len; i++) {
+        if (nums[i] === 0) {
+            p = 1
+            q = 1
+            found = false
+            res = Math.max(res, 0)
+            continue
+        }
+        p *= nums[i]
+        res = Math.max(res, p)
+        if (found) {
+            q *= nums[i]
+            res = Math.max(res, q)
+        }
+        if (!found && nums[i] < 0) {
+            found = true
+        }
+    }
+    return 
+};
+
+
+var maxProduct1 = function(nums) {
+    const len = nums.length
+    let res = Number.NEGATIVE_INFINITY, max = 1, min = 1
+    for (let i = 0; i < len; i++) {
         const v = nums[i]
         if (v < 0) {
             [max, min] = [min, max]
@@ -18,4 +44,5 @@ var maxProduct = function (nums) {
 };
 
 
-// console.log(maxProduct([2,3,-2,4,2,9,-1,2,3,-3,6,-2,0,1,2,3, - 3, 4]))
+
+console.log(maxProduct([2,3,-2,4,2,9,-1,2,3,-3,6,-2,0,1,2,3, -3, 4,5,-2,1,3,-3,-3,9,2]))
